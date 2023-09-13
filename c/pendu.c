0e0c7f3820d066b3c0c8a84efc483a28fb87db42
Lucas <lucasbarbr@outlook.com>
2023-09-13 15:52:04 +0200

    simplifications fct printMotMystere et utilisation strlen

--- c/pendu.c
+++ c/pendu.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<ctype.h>
 
 
 void print_bienvenue();
-int size_string(char mot[]);
+// int size_string(char mot[]); IL existe strlen dans la bibliothèque string.h
 char lireCaractere();
 int findLetter(char letter, int lenMot, int count_found, char letters_found[]);
 void printMotMystere(int lenMot, char letters_found[]);
+int nombreLettresTrouvees(char motsecret[], char lettres_trouvees[]);
 
-char mot_mystere[] = "LUCAS";
+char mot_mystere[] = "LUCASBARBIER";
 
 int main() {
     // int indices_found[] = {};
@@ -18,9 +20,15 @@ int main() {
     int isLetterPresent = 0;
     int current_count, total_count = 0;
     char* letters_found = NULL;
-    int len_mot = size_string(mot_mystere);
+    int len_mot = strlen(mot_mystere);
     letters_found = (char*) malloc(len_mot * sizeof(char));
 
+    if(letters_found == NULL){
+        exit(0);
+    }
+    // for (int i = 0; i < len_mot; i++){
+    //     letters_found[i] = 0;
+    // }
     print_bienvenue();
     printf("Le fait mystère fait %d lettres\n", len_mot);
 
@@ -29,38 +37,46 @@ int main() {
         printf("Entrer votre lettre: ");
         currentchar = lireCaractere();
         isLetterPresent = findLetter(currentchar, len_mot, total_count, letters_found);
+
     if (isLetterPresent == 1) { // La lettre n'est pas dans le mot
         viesRestante -= 1;
     }
-    else if (isLetterPresent == 0) {
-        total_count += 1;
-        // printf("Compte total:%d", total_count);
-    }
 
         printMotMystere(len_mot, letters_found);
-        if (total_count == len_mot){
-        printf("Félicitations, le mot est trouvé !\n");
+        if (nombreLettresTrouvees(mot_mystere, letters_found) == len_mot){
+        printf("Félicitations, le mot %s est trouvé !\n", mot_mystere);
         break;
     }
     }
-    
+    free(letters_found);
     return 0;
 }
 
 
 void print_bienvenue(){
-    printf("Bievenue dans le jeu du pendu en C !\n");
+    printf("Bienvenue dans le jeu du pendu en C !\n");
     printf("Vous avez 10 vies pour déterminer le mot mystère...\n");
 }
 
-int size_string(char mot[]) {
-    int i = 0;
-    while (mot[i] != '\0'){
-        i++;
+int nombreLettresTrouvees(char motsecret[], char lettres_trouvees[]){
+    int compteur = 0;
+    for (int i = 0; i < strlen(motsecret); i++){
+        if (motsecret[i] == lettres_trouvees[i]){
+            compteur += 1;
+        }
     }
-    return i;
-
+    return compteur;
 }
+
+
+// int size_string(char mot[]) {
+//     int i = 0;
+//     while (mot[i] != '\0'){
+//         i++;
+//     }
+//     return i;
+
+// }
 
 char lireCaractere() {
     char caractere = 0;
@@ -74,7 +90,6 @@ char lireCaractere() {
 }
 
 int findLetter(char letter, int lenMot, int count_found, char letters_found[]){
-    int count = count_found;
     int isLetterPresent = 1;
     // Verifie si la lettre & déjà été entrée
     for(int i = 0; i <= lenMot; i++){
@@ -84,8 +99,7 @@ int findLetter(char letter, int lenMot, int count_found, char letters_found[]){
     }
     for(int i = 0; i <= lenMot; i++){
         if (mot_mystere[i] == letter){
-            letters_found[count] = letter;
-            count++; // Prendre en compte quand plusieurs lettres similaires...
+            letters_found[i] = letter;
             isLetterPresent = 0;
         }
     }
@@ -95,16 +109,12 @@ int findLetter(char letter, int lenMot, int count_found, char letters_found[]){
 void printMotMystere(int lenMot, char letters_found[]){
     int found = 1;
     for(int i = 0; i <= lenMot; i++){
-        found = 1;
-        for(int j = 0; j <= lenMot; j++){
-            if(mot_mystere[i] == letters_found[j]){
+            if(mot_mystere[i] == letters_found[i]){
                 printf("%c", mot_mystere[i]);
-                found = 0;
+            }
+            else {
+                printf("%c",'*');
             }
         }
-        if (found == 1){
-        printf("%c", '*');
-        }
-    }
     printf("\n");
 }
