a17f0b91e6ecf2f36830a18048eabb0a691ef4b3
Lucas <lucasbarbr@outlook.com>
2023-09-14 15:50:01 +0200

    creation main.h

---
+++ c/app/main.h
@@ -0,0 +1,8 @@
+#ifndef DEF_MAIN
+#define DEF_MAIN
+
+char lireCaractere();
+int findLetter(char letter, char letters_found[]);
+int nombreLettresTrouvees(char motsecret[], char lettres_trouvees[]);
+
+#endif
