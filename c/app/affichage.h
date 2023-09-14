37f150a53cce7a67fc43b64ce245653ad3da1d01
Lucas <lucasbarbr@outlook.com>
2023-09-14 15:50:24 +0200

    replacement def foctions dans #if

--- c/app/affichage.h
+++ c/app/affichage.h
@@ -1,6 +1,9 @@
-#ifndef AFFICHAGE
-#define AFFICHAGE
-#endif
+#ifndef DEF_AFFICHAGE
+#define DEF_AFFICHAGE
 
 void printMotMystere(char motmystere[], char letters_found[]);
 void print_bienvenue();
+
+#endif
+
+
