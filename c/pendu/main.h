6969051562109d4b49d568b27674e438c11ce50c
lukbrb <lucasbarbr@outlook.com>
2023-09-16 21:10:08 +0200

    ajout arg pour afficher couleur jaune dans lire caractere

--- c/pendu/main.h
+++ c/pendu/main.h
@@ -1,8 +1,10 @@
 #ifndef DEF_MAIN
 #define DEF_MAIN
 
-char lireCaractere();
+char lireCaractere(int color);
 int findLetter(char letter, char mot_mystere[], char letters_found[]);
 int nombreLettresTrouvees(char motsecret[], char lettres_trouvees[]);
+char* choix_langage();
+void print_langages();
 
 #endif
