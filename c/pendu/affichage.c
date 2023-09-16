da437e7d825c86a81616e1698eb8339f00828958
lukbrb <lucasbarbr@outlook.com>
2023-09-16 17:53:02 +0200

    ajout print pendu

--- c/pendu/affichage.c
+++ c/pendu/affichage.c
@@ -26,3 +26,29 @@ void printMotMystere(char motmystere[], char letters_found[]){
         }
     printf("\n\n");
 }
+
+int print_pendu(int index_print)
+{
+    FILE* fichier = NULL;
+    int caractere_lu = 0, nb_caracteres = 0;
+    char* filename = "media/dessin.txt";
+
+    fichier = fopen(filename, "r");
+
+    if (fichier == NULL){
+        printf(ROUGE "[x] Impossible d'ouvrir le fichier '%s'\n" RESET, filename);
+        return 0;
+    }
+
+    do {
+        caractere_lu = fgetc(fichier);
+        printf(BLANC "%c" RESET, caractere_lu);
+        if (caractere_lu != '\n' && caractere_lu != ' ') nb_caracteres++;
+    } while(caractere_lu != EOF && nb_caracteres < index_print);
+
+    printf("\n");
+    fclose(fichier);
+
+    return 1;
+
+}
