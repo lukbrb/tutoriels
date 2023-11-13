961bdbd68b22414bc82cfe7097962543b38e3691
lukbrb <lucasbarbr@outlook.com>
2023-11-13 19:41:39 +0100

    code pour maintenir une fenetre ouverte

--- fortran/tic-tac-toe/app/main.f90
+++ fortran/tic-tac-toe/app/main.f90
@@ -1,8 +1,19 @@
 program main
-  use tic_tac_toe, only: say_hello
   use c_interface
+  use iso_c_binding, only: c_int
   implicit none
 
-  call say_hello()
-  call hello_c()
+  integer(kind=c_int) :: width, height, fps
+
+  width = 600
+  height = 800
+  fps = 60
+
+  call init_window(width, height, "Fenetre Raylib"//c_null_char)
+
+  call set_target_fps(fps)
+  do while(.not. window_should_close())
+    call begin_drawing()
+    call end_drawing()
+  end do
 end program main
