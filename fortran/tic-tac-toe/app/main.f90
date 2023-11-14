57c3492c14fed2f90b61ae3fdd204a67da585164
lukbrb <lucasbarbr@outlook.com>
2023-11-14 20:47:39 +0100

    add struct et couleur de fond

--- fortran/tic-tac-toe/app/main.f90
+++ fortran/tic-tac-toe/app/main.f90
@@ -1,19 +1,21 @@
 program main
   use c_interface
-  use iso_c_binding, only: c_int
+  use iso_c_binding, only: c_int, c_int32_t
   implicit none
 
   integer(kind=c_int) :: width, height, fps
+  integer(c_int32_t) :: bleu
 
-  width = 600
-  height = 800
+  width = 800
+  height = 600
   fps = 60
-
+  bleu = int(z'FFF17900', c_int32_t)
   call init_window(width, height, "Fenetre Raylib"//c_null_char)
 
   call set_target_fps(fps)
   do while(.not. window_should_close())
     call begin_drawing()
+    call clear_background(bleu)
     call end_drawing()
   end do
 end program main
--- fortran/tic-tac-toe/src/c-interface.f90
+++ fortran/tic-tac-toe/src/c-interface.f90
@@ -1,6 +1,11 @@
 module c_interface
-    use iso_c_binding, only : c_char, c_null_char, c_int, c_bool
-    
+    use iso_c_binding, only : c_char, c_null_char, c_int, c_bool, c_int32_t
+  
+        type, bind(c) :: Color
+            integer(c_int) :: i1, i2
+            character(kind=c_char) :: str(5)
+        end type Color
+
     interface
         subroutine init_window(width, height, title) bind (C, name="InitWindow")
             import c_char, c_int
@@ -22,6 +27,11 @@ module c_interface
         end subroutine begin_drawing
         subroutine end_drawing() bind(C, name="EndDrawing")
         end subroutine end_drawing
+
+        subroutine clear_background(color) bind(C, name="ClearBackground")
+            import c_int32_t
+            integer(c_int32_t), value :: color
+        end subroutine clear_background
     end interface
 
 end module c_interface
